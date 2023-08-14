862ed04ef202342ddcba93d10e81d6e89277d2c4
tanmoon20 <tanseemoon@gmail.com>
2023-08-14 17:44:01 +0800

    update all

--- app/src/main/java/my/edu/utar/sweetzmobileapp/activity_splash_screen.java
+++ app/src/main/java/my/edu/utar/sweetzmobileapp/activity_splash_screen.java
@@ -7,7 +7,7 @@ import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.view.animation.Animation;
