118755d7bd49941390f7cbad00fccde259d4cce0
tanmoon20 <tanseemoon@gmail.com>
2023-08-14 16:46:18 +0800

    convert from androidv7 to androidx

--- app/src/main/java/my/edu/utar/sweetzmobileapp/MainActivity.java
+++ app/src/main/java/my/edu/utar/sweetzmobileapp/MainActivity.java
@@ -1,6 +1,6 @@
 package my.edu.utar.sweetzmobileapp;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/my/edu/utar/sweetzmobileapp/activity_splash_screen.java
+++ app/src/main/java/my/edu/utar/sweetzmobileapp/activity_splash_screen.java
@@ -5,7 +5,7 @@ import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.view.animation.Animation;
