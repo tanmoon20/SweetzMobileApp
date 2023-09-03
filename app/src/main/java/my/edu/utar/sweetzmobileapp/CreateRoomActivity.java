962f4b16ba79e08a8264bc2cf42b66dcca701d9e
ToucheCarrot <tayyanbin0740@1utar.my>
2023-09-03 11:14:44 +0800

    add login, mute_btn

--- app/src/main/java/my/edu/utar/sweetzmobileapp/CreateRoomActivity.java
+++ app/src/main/java/my/edu/utar/sweetzmobileapp/CreateRoomActivity.java
@@ -1,15 +1,8 @@
 package my.edu.utar.sweetzmobileapp;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-public class CreateRoomActivity extends HeaderFooterActivity {
-
-    FloatingActionButton mute;
-    TextView text;
+ public class CreateRoomActivity extends HeaderFooterActivity {
 
      public CreateRoomActivity()
      {
@@ -20,31 +13,5 @@ public class CreateRoomActivity extends HeaderFooterActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_room);
-
-        mute = findViewById(R.id.mute);
-        text = findViewById(R.id.fab_text);
-
-        mute.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MusicManager musicManager = MusicManager.getInstance();
-                musicManager.toggleMute();
-
-                if (musicManager.isMuted()) {
-                    mute.setImageResource(R.drawable.music_off);
-                } else {
-                    mute.setImageResource(R.drawable.music_on);
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        MusicManager musicManager = MusicManager.getInstance();
-        if (musicManager.isMuted()) {
-            musicManager.toggleMute();
-        }
     }
 }
